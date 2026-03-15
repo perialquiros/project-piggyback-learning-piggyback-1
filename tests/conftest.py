6fc49abae2ced702f9c06b3abae71743d8df7a6c
Jerry Lin <jerryxue0717@gmail.com>
2026-03-15 02:50:59 -0400

    test: isolate test from real DB using temp SQLite file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import os
 from pytest_html import extras
+import tempfile
 #set a fake key before pytest import anything from the app
 os.environ["OPENAI_API_KEY"] = "fake-key-for-testing"
 
@@ -10,3 +11,12 @@ def pytest_html_report_title(report):
 
 def pytest_configure(config):
     config._metadata = {}
+    
+#must happen before any app imports read settings
+temp = tempfile.NamedTemporaryFile(suffix=".db", delete=False)
+os.environ["SQLITE_PATH"] = temp.name
+
+def pytest_sessionfinish(session,exitstatus):
+    path = os.environ.get("SQLITE_PATH")
+    if path and "tmp" in path:
+        os.unlink(path)
